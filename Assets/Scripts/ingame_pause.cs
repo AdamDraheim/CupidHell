1f3ab6991d7968c24467cf04f826632937012142
AdamDraheim <acdraheim@gmail.com>
2020-02-29 01:15:40 -0700

    Made in game pause menu

---
+++ Assets/Scripts/ingame_pause.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ingame_pause : MonoBehaviour
+{
+
+    public GameObject obj;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            pause();
+        }
+    }
+
+    public void pause()
+    {
+        obj.active = true;
+        Time.timeScale = 0;
+    }
+
+    public void unpause()
+    {
+        Time.timeScale = 1;
+        obj.active = false;
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(name);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
