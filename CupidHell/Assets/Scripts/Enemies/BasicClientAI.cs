c01e6cc27e54086da11439703f5473298071bfb4
Ethan Bowen <Ethankbowen@gmail.com>
2020-02-28 19:31:14 -0700

    Base enemie implentation, health and taking damage

--- CupidHell/Assets/Scripts/Enemies/BasicClientAI.cs
+++ CupidHell/Assets/Scripts/Enemies/BasicClientAI.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class BasicClientAI : MonoBehaviour
 {
+    private float health;
+    //public int Health { get => health; set => health = value; }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        health = 10;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    public void Hurt()
+    {
+        float damage = 1.0f;
+        health -= damage;
+    }
+
+    void OnCollisonEnter(GameObject other)
+    {
+        if (other.tag.Equals("Bullet"))
+        {
+            Hurt();
+            Destroy(other);
+        }
     }
 }
---
+++ CupidHell/Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour
+{
+    private int health;
+
+    public int Health { get { return health; } set => health = value; }
+
+    public abstract void Hurt();
+}
