4f2db2d70f2b5cc1f66c19d5524e9b0805b8afa3
James Youngblood <u1235424@ad.utah.edu>
2020-02-28 20:04:19 -0700

    Add health and delete collisions to Cover

--- CupidHell/Assets/Scripts/MoveSphereTemp.cs
+++ CupidHell/Assets/Scripts/MoveSphereTemp.cs
@@ -2,18 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveBullet : MonoBehaviour
+public class MoveSphereTemp : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(new Vector3(0, 0, -1f) * Time.deltaTime);
-        if (transform.position)
+        transform.Translate(Vector3.forward * Time.deltaTime * 3);
+    }
+    
+    void OnCollisionEnter(Collision collision)
+    {
+        transform.position = new Vector3(0, 0, -4f);
     }
 }
