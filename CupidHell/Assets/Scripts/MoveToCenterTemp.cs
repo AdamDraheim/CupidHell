f70ed6f9e3c9bc0b5af01a34063b19564c1d3a21
James Youngblood <james@youngbloods.org>
2020-02-28 22:34:54 -0700

    Add spawning method for any enemy prefab

---
+++ CupidHell/Assets/Scripts/MoveToCenterTemp.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToCenterTemp : MonoBehaviour
+{
+    private Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.Find("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float step = 1.0f * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+}
