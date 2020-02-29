01f51c2c1732b05b75311f6c1d24b271652997bd
James Youngblood <james@youngbloods.org>
2020-02-28 23:06:16 -0700

    Add power up spawning process

---
+++ CupidHell/Assets/Scripts/PowerUpSpawning.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpSpawning : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public float spawnInterval = 8.0f;
+    public float screenBoundX = 10.0f;
+    public float screenBoundZ = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(powerUpWave());
+    }
+
+    private void spawnPowerUp()
+    {
+        GameObject enemy = Instantiate(enemyPrefab) as GameObject;
+
+        float x = Random.Range(-screenBoundX, screenBoundX);
+        float z = Random.Range(-screenBoundZ, screenBoundZ);
+
+        enemy.transform.position = new Vector3(x, 10.0f, z);
+    }
+
+    IEnumerator powerUpWave()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnInterval);
+            spawnPowerUp();
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
